59bb3f7599bf7a932e5dd727c36a27b40a4e64b0
Gil <sgy@tpay.co.kr>
2020-06-14 19:58:25 +0900

    [Add] Add AbstractUser

---
+++ sources/app/config/settings/base/authentication.py
@@ -0,0 +1,4 @@
+# https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
+AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
+# https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
+AUTH_USER_MODEL = "users.User"
