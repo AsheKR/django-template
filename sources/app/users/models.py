38cb3c0900c469b12c48a36dab49cee44c7bef75
Gil <sgy@tpay.co.kr>
2020-06-21 17:09:22 +0900

    [Add] Add User's Name

--- sources/app/users/models.py
+++ sources/app/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db.models import CharField
+from django.utils.translation import ugettext_lazy as _
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    name = CharField(_("Name of User"), blank=True, max_length=255)
