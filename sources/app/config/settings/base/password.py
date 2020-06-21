a7e7c1637480ae3664bccdf37f47f382f325dbe8
Gil <sgy@tpay.co.kr>
2020-06-21 15:20:43 +0900

    [Fix] typo

--- sources/app/config/settings/base/password.py
+++ sources/app/config/settings/base/password.py
@@ -12,5 +12,5 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.passwrd_validation.NumericPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
