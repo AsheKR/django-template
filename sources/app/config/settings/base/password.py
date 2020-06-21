b914a412012c7f8569d7acdd368e90cff6469bb1
Gil <sgy@tpay.co.kr>
2020-06-21 17:10:26 +0900

    [Reword] Change the inner comment to the outside

--- sources/app/config/settings/base/password.py
+++ sources/app/config/settings/base/password.py
@@ -1,5 +1,5 @@
+# https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
 PASSWORD_HASHERS = [
-    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
